74a0cb31d3fa46fdab7f803fb728048127e1aa16
sebastian <sarrietav.dll@gmail.com>
2020-07-02 00:14:18 -0500

    Maven transcition part 2

--- src/logic/file_management/borrower_crud/DeleteBorrower.java
+++
@@ -1,6 +0,0 @@
-package logic.file_management.borrower_crud;
-
-import logic.file_management.CRUD;
-
-public class DeleteBorrower extends CRUD {
-}
--- src/logic/file_management/loan_crud/DeleteLoan.java
+++
@@ -1,6 +0,0 @@
-package logic.file_management.loan_crud;
-
-import logic.file_management.CRUD;
-
-public class DeleteLoan extends CRUD {
-}
