e567a9e6bd7bbb2b9e57c4c02f98209fe06bf403
sebastian <sarrietav.dll@gmail.com>
2020-07-04 14:49:39 -0500

    Modified Employee

--- src/main/java/logic/Employee.java
+++ src/main/java/logic/Employee.java
@@ -1,14 +1,15 @@
 package logic;
 
 public class Employee {
-    private String username;
+    private String id;
     private String password;
 
     private String name;
     private String homePhone;
     private String mobilePhone;
     private String address;
-    private double salary;
 
+    private double baseSalary;
+    private double currentSalary;
 
 }
