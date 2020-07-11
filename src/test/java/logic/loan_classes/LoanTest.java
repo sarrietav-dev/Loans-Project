48719c45b8a09aa5032df1c3169ec783cb92f999
Reimboulk <60827552+Reimboulk@users.noreply.github.com>
2020-07-11 13:36:01 -0500

    Added Design 2.0

---
+++ src/test/java/logic/loan_classes/LoanTest.java
@@ -0,0 +1,17 @@
+package logic.loan_classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoanTest {
+    @Test
+    void amountIllegalArgumentExceptionTest() {
+        assertThrows(IllegalArgumentException.class, () -> new Loan(-1, new Client(), "2020-06-22"));
+    }
+
+    @Test
+    void incorrectDateOrderTest() {
+        assertThrows(IllegalArgumentException.class, () -> new Loan(-1, new Client(), "22-06-2020"));
+    }
+}
