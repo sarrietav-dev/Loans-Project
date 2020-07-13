8807f5890f9674a0e305c5fbb785425a951aa6e8
sebastian <sarrietav.dll@gmail.com>
2020-07-13 16:39:17 -0500

    Added documentation

--- src/main/java/logic/file_management/client_crud/DeleteClient.java
+++ src/main/java/logic/file_management/client_crud/DeleteClient.java
@@ -1,14 +1,6 @@
 package logic.file_management.client_crud;
 
-import logic.company_members.Employee;
 import logic.file_management.CRUD;
-import logic.loan_classes.Client;
-import logic.loan_classes.Loan;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class DeleteClient extends CRUD {
 }
