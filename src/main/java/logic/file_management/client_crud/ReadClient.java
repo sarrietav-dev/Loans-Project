6780497501488430859bf5b8899220d30d44d393
sebastian <sarrietav.dll@gmail.com>
2020-07-14 11:44:53 -0500

    Added getLoans

--- src/main/java/logic/file_management/client_crud/ReadClient.java
+++ src/main/java/logic/file_management/client_crud/ReadClient.java
@@ -1,10 +1,13 @@
 package logic.file_management.client_crud;
 
+import logic.exceptions.ObjectNotFoundException;
 import logic.file_management.CRUD;
 import logic.file_management.loan_crud.ReadLoan;
 import logic.loan_classes.Client;
 import logic.loan_classes.Loan;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -42,5 +45,16 @@ public class ReadClient extends CRUD {
         return getAllClients().stream()
                 .filter(ReadClient::hasAnyLoanDelayed)
                 .collect(Collectors.toSet());
+    }
+
+    public static ArrayList<Loan> getLoans(Client client) {
+	    return data.get(client);
+    }
+
+    public static ArrayList<Loan> getLoans(final int CLIENT_ID) {
+	    for (Client client : getAllClients())
+	        if (client.getId() == CLIENT_ID)
+	            return data.get(client);
+        throw new ObjectNotFoundException("Client not found!");
     }
 }
